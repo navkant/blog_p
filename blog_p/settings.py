3292919c4215001e4a1eae15056169f533135875
navkant <navkant.tyagi9@gmail.com>
2025-01-01 11:42:18 +0530

    added allowed host

--- blog_p/settings.py
+++ blog_p/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-73s@l78ao)=^l^e(b&bmv_68yayla5kzv9$i9ou-q(8a*7z)&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.8', '*']
+ALLOWED_HOSTS = ['21f0z1gdni.execute-api.us-east-1.amazonaws.com']
 
 
 # Application definition
