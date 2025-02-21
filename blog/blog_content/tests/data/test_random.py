f609d4c18db2099ddfd119e389b4e90e056268f9
navkant <navkant.tyagi9@gmail.com>
2025-02-21 10:17:32 +0530

    removed the sleep statement, test database checked

--- blog/blog_content/tests/data/test_random.py
+++ blog/blog_content/tests/data/test_random.py
@@ -1,10 +1,9 @@
 from django.test.testcases import TestCase, SimpleTestCase
-import  time
+
 
 class FirstDjangoTest(TestCase):
     def setUp(self):
         pass
 
     def test_assertion(self):
-        time.sleep(60)
         self.assertEquals(1, 1)
