5a52f985d7de427a3676bf8f0160768cb55fdfb3
navkant <navkant.tyagi9@gmail.com>
2025-02-21 10:11:43 +0530

    checking if test database is being created or not

--- blog/blog_content/tests/data/test_random.py
+++ blog/blog_content/tests/data/test_random.py
@@ -1,9 +1,10 @@
 from django.test.testcases import TestCase, SimpleTestCase
-
+import  time
 
 class FirstDjangoTest(TestCase):
     def setUp(self):
         pass
 
     def test_assertion(self):
+        time.sleep(60)
         self.assertEquals(1, 1)
